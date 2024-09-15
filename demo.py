642ddef6ca173ee1f5eeff27e92578b8b7884bd0
MyWorksHarshaNaik <naikharshas235@gmail.com>
2024-09-15 12:44:43 +0530

    utiliti added

---
+++ demo.py
@@ -0,0 +1,10 @@
+from us_visa.logger import logging
+from us_visa.exception import USvisaException
+import sys
+
+# logging.info("custom log")
+
+try:
+    a = 2/0
+except Exception as e:
+    raise USvisaException(e, sys)
