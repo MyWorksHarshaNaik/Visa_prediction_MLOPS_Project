bde3e07a8d8c8f6518933e31db2573b19899036f
MyWorksHarshaNaik <naikharshas235@gmail.com>
2024-09-25 20:20:38 +0530

    data ingestion added

--- README.md
+++ README.md
@@ -19,3 +19,14 @@ conda activate visa
 ```bash
 pip install -r requirements.txt
 ```
+
+## Workflow:
+1. constants
+2. entity
+3. components
+4. pipeline
+5. Main file
+
+
+
+
--- demo.py
+++ demo.py
@@ -1,10 +1,5 @@
-from us_visa.logger import logging
-from us_visa.exception import USvisaException
-import sys
+from us_visa.pipline.training_pipeline import TrainPipeline
 
-# logging.info("custom log")
 
-try:
-    a = 2/0
-except Exception as e:
-    raise USvisaException(e, sys)
+obj = TrainPipeline()
+obj.run_pipeline()
